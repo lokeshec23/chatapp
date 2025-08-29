581763199af0936a8893db3ecb7333ca0ce2d504
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2025-08-29 15:17:27 +0530

    mongodb config

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-// index.js
+// server/index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js"; // Import the db connection function
 
 // Load environment variables
 dotenv.config();
+
+// Connect to the database
+connectDB();
 
 // Initialize the express app
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware setup
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-app.use(express.json()); // Enable the express app to parse JSON formatted request bodies
+app.use(cors());
+app.use(express.json());
 
 // A simple test route
 app.get("/", (req, res) => {
